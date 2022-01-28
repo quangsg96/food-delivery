104176210d23dbba35ee6090b7007b41387cdb46
quangsg96 <minhquang96@protonmail.com>
2022-01-28 21:57:23 +0700

    upload source code

---
+++ js/interaction.js
@@ -0,0 +1,6 @@
+document.getElementById("menu__open").addEventListener("click", function () {
+  document.getElementById("menu").style.right = "0";
+});
+document.getElementById("menu__close").addEventListener("click", function () {
+  document.getElementById("menu").style.right = "-100%";
+});
